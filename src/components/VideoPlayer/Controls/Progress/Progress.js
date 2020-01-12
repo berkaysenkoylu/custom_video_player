b261fc6e5c9dcd621c70f4b96b05923a302ddea3
berkaysenkoylu <senkoylu_berkay@yahoo.com>
2020-01-12 23:21:07 +0300

    added progress bar

--- src/components/VideoPlayer/Controls/Progress/Progress.js
+++ src/components/VideoPlayer/Controls/Progress/Progress.js
@@ -1,11 +1,79 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 
 import classes from './Progress.module.scss';
 
 const Progress = (props) => {
+    const [isMouseDown, setIsMouseDown] = useState(false);
     const [posX, setPosX] = useState(0);
 
     let backgroundRef = useRef(null);
+
+    useEffect(() => {
+        if(props.progressValue) {
+            setPosX(posX => props.progressValue);
+        }
+    }, [props.progressValue]);
+
+    const findPosition = (event) => {
+        let rect = backgroundRef.current.getBoundingClientRect();
+        let rectWidth = rect.width;
+        let leftPosPercentage = ((event.pageX - rect.left) / rectWidth) * 100;
+
+        if(leftPosPercentage > 100) {
+            leftPosPercentage = 100;
+        }
+
+        if(leftPosPercentage < 0) {
+            leftPosPercentage = 0;
+        }
+
+        return Math.floor(leftPosPercentage);
+    }
+
+    const onMouseDownHandlerCB = useCallback((event) => {
+        if(event.target !== backgroundRef.current) {
+            return;
+        }
+
+        setIsMouseDown(isMouseDown => true);
+
+        let position = findPosition(event);
+
+        setPosX(position);
+    }, []);
+
+    const onMouseMoveHandlerCB = useCallback((event) => {
+        if(isMouseDown) {
+            let position = findPosition(event);
+
+            setPosX(position);
+        }
+    }, [isMouseDown]);
+
+    const onMouseUpHandlerCB = useCallback((event) => {
+        if(!isMouseDown) {
+            return;
+        }
+
+        setIsMouseDown(isMouseDown => false);
+
+        let position = findPosition(event);
+
+        setPosX(position);
+    }, [isMouseDown]);
+
+    useEffect(() => {
+        document.addEventListener('mousedown', onMouseDownHandlerCB);
+        document.addEventListener('mouseup', onMouseUpHandlerCB);
+        document.addEventListener('mousemove', onMouseMoveHandlerCB);
+
+        return () => {
+            document.removeEventListener('mousedown', onMouseDownHandlerCB);
+            document.removeEventListener('mouseup', onMouseUpHandlerCB);
+            document.removeEventListener('mousemove', onMouseMoveHandlerCB);
+        };
+    }, [onMouseDownHandlerCB, onMouseMoveHandlerCB, onMouseUpHandlerCB]);
+
 
     let foregroundStyle = {
         left: `${0}%`,
