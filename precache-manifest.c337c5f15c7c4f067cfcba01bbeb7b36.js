35982f70f8811e6d208cfbb233a02855feec843a
berkaysenkoylu <senkoylu_berkay@yahoo.com>
2020-01-24 17:06:50 +0300

    Updates

---
+++ precache-manifest.c337c5f15c7c4f067cfcba01bbeb7b36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "952ae0357594308225deff1e8627c0e4",
+    "url": "/custom_video_player/index.html"
+  },
+  {
+    "revision": "ced90f330455c737f861",
+    "url": "/custom_video_player/static/css/main.975ba8dd.chunk.css"
+  },
+  {
+    "revision": "574d5bd1572450427eb3",
+    "url": "/custom_video_player/static/js/2.aa401b20.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/custom_video_player/static/js/2.aa401b20.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ced90f330455c737f861",
+    "url": "/custom_video_player/static/js/main.2bd0491f.chunk.js"
+  },
+  {
+    "revision": "9407b21e93d1cb831588",
+    "url": "/custom_video_player/static/js/runtime-main.06cf03f6.js"
+  },
+  {
+    "revision": "dbcf112aeb7e995bd1c2085400e00a3d",
+    "url": "/custom_video_player/static/media/sprite.dbcf112a.svg"
+  }
+]);
